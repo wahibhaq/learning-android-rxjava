fbce04e8560a4dd86fb8d1425a9fb539d4230e97
Wahib-Ul-Haq <wahib.tech@gmail.com>
2017-08-23 08:46:10 +0200

    Transforemd fetching gist file as per rxjava2

--- casterio/src/main/java/com/learning/rxjava/fragments/IntroFragment.java
+++ casterio/src/main/java/com/learning/rxjava/fragments/IntroFragment.java
@@ -17,16 +17,19 @@ import com.learning.rxjava.models.GistFile;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import rx.Observable;
-import rx.Subscriber;
-import rx.Subscription;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Func0;
-import rx.schedulers.Schedulers;
 
 /**
  * Created by wahibulhaq on 17/04/16.
@@ -35,7 +38,7 @@ public class IntroFragment extends Fragment {
 
     private static final String TAG = IntroFragment.class.getSimpleName();
 
-    private Subscription subscription;
+    private Disposable subscription;
 
     public static IntroFragment newInstance() {
         return new IntroFragment();
@@ -45,47 +48,49 @@ public class IntroFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Function<Gist, StringBuilder> outputToDisplay = new Function<Gist, StringBuilder>() {
+            @Override
+            public StringBuilder apply(@NonNull Gist gist) throws Exception {
+                StringBuilder sb1 = new StringBuilder();
+                for (Map.Entry<String, String> owner : gist.owner.entrySet()) {
+                    if(owner.getKey().equals("login")) {
+                        sb1.append(owner.getKey());
+                        sb1.append(" : ");
+                        sb1.append(owner.getValue());
+                    }
+                }
+
+                sb1.append("\n\n\n");
+
+                for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
+                    sb1.append(entry.getKey());
+                    sb1.append(" - ");
+                    sb1.append("Length of file ");
+                    sb1.append(entry.getValue().size);
+                    sb1.append("\n");
+                }
+                return sb1;
+            }
+        };
+
         //subscribing
         subscription = getGistObservable()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Gist>() {
+                .map(outputToDisplay)
+                .doOnError(new Consumer<Throwable>() {
                     @Override
-                    public void onCompleted() {
-
+                    public void accept(@NonNull Throwable throwable) throws Exception {
+                        Log.e(TAG, throwable.getCause().getMessage());
                     }
-
+                })
+                .subscribe(new Consumer<StringBuilder>() {
                     @Override
-                    public void onError(Throwable e) {
-                        Log.e(TAG, e.getMessage(), e);
-                    }
-
-                    @Override
-                    public void onNext(Gist gist) {
-                        if (gist != null) {
-
-                            StringBuilder sb1 = new StringBuilder();
-                            for (Map.Entry<String, String> owner : gist.owner.entrySet()) {
-                                if(owner.getKey().equals("login")) {
-                                    sb1.append(owner.getKey());
-                                    sb1.append(" : ");
-                                    sb1.append(owner.getValue().toString());
-                                }
-                                break;
-                            }
-
-                            StringBuilder sb = new StringBuilder();
-                            //Output
-                            for (Map.Entry<String, GistFile> entry : gist.files.entrySet()) {
-                                sb.append(entry.getKey());
-                                sb.append(" - ");
-                                sb.append("Length of file ");
-                                sb.append(entry.getValue().size.toString());
-                                sb.append("\n");
-                            }
-
-                            TextView textView = (TextView) getView().findViewById(R.id.main_message);
-                            textView.setText(sb1.toString() + "\n\n" + sb.toString());
+                    public void accept(@NonNull StringBuilder stringBuilder) throws Exception {
+                        if(getView().findViewById(R.id.main_message) != null) {
+                            TextView textView = (TextView) getView()
+                                    .findViewById(R.id.main_message);
+                            textView.setText(stringBuilder.toString());
                         }
                     }
                 });
@@ -111,29 +116,26 @@ public class IntroFragment extends Fragment {
         Response response = client.newCall(request).execute();
 
         if(response.isSuccessful()) {
-            Gist gist = new Gson().fromJson(response.body().charStream(), Gist.class);
-            return gist;
+            return new Gson().fromJson(response.body().charStream(), Gist.class);
+        } else {
+            return null;
         }
-
-        return null;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(subscription != null && !subscription.isUnsubscribed()) {
-            subscription.unsubscribe(); //In order to provide memory leak
+        if(subscription != null && !subscription.isDisposed()) {
+            subscription.dispose(); //In order to avoid memory leak
         }
-
     }
 
 
 
     public Observable<Gist> getGistObservable() {
-
-        return Observable.defer(new Func0<Observable<Gist>>() {
+        return Observable.defer(new Callable<ObservableSource<? extends Gist>>() {
             @Override
-            public Observable<Gist> call() {
+            public ObservableSource<? extends Gist> call() throws Exception {
                 try {
                     return Observable.just(getGist()); //gets called only after subscriber
                 } catch (IOException e) {
